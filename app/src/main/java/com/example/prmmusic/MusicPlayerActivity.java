50b171c6491559cb80c8c8ff2e3c412ff8792e86
G2KSven <huynqhe140638@fpt.edu.vn>
2021-10-24 21:50:21 +0700

    modify playlist fragment + edit

--- app/src/main/java/com/example/prmmusic/MusicPlayerActivity.java
+++ app/src/main/java/com/example/prmmusic/MusicPlayerActivity.java
@@ -2,17 +2,16 @@ package com.example.prmmusic;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
-import android.media.MediaParser;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,21 +28,30 @@ public class MusicPlayerActivity extends AppCompatActivity {
     private SeekBar sb_playseekbar;
     private MediaPlayer mediaPlayer;
     private Handler handler = new Handler();
-    @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        setContentView(R.layout.activity_musicplayer);
-        imv_playandpause = findViewById(R.id.imv_PlayAndPause);
-        tv_currenttime = findViewById(R.id.tv_currentime);
-        tv_totalduration = findViewById(R.id.tv_totalduration);
-        sb_playseekbar = findViewById(R.id.sb_playseekbar);
-        imvc_dianhac = findViewById(R.id.imvc_dianhac);
+    public void animatorSetup(){
         objectAnimator = ObjectAnimator.ofFloat(imvc_dianhac, "rotation", 0f, 360f);
         objectAnimator.setDuration(10000);
         objectAnimator.setRepeatCount(ValueAnimator.INFINITE);
         objectAnimator.setRepeatMode(ValueAnimator.RESTART);
         objectAnimator.setInterpolator(new LinearInterpolator());
         objectAnimator.start();
+        objectAnimator.pause();
+    }
+    public void init(){
+        imv_playandpause = findViewById(R.id.imv_PlayAndPause);
+        tv_currenttime = findViewById(R.id.tv_currentime);
+        tv_totalduration = findViewById(R.id.tv_totalduration);
+        sb_playseekbar = findViewById(R.id.sb_playseekbar);
+        imvc_dianhac = findViewById(R.id.imvc_dianhac);
+    }
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.activity_musicplayer);
+        //find id for component
+        init();
+        //set up animator for music CD on activity
+        animatorSetup();
         mediaPlayer = new MediaPlayer();
 
         sb_playseekbar.setMax(100);
@@ -52,21 +60,57 @@ public class MusicPlayerActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(mediaPlayer.isPlaying()){
+                    objectAnimator.pause();
                     handler.removeCallbacks(updater);
                     mediaPlayer.pause();
                     imv_playandpause.setImageResource(R.drawable.ic_play);
 
+
                 }else{
+                    objectAnimator.resume();
                     mediaPlayer.start();
                     imv_playandpause.setImageResource(R.drawable.ic_pause);
                     updateSeekBar();
+
                 }
             }
         });
+
         String urlSong = "https://firebasestorage.googleapis.com/v0/b/prmmusiclistener.appspot.com/o/Music%2Ftest.mp3?alt=media&token=60a18dc9-25f4-4c33-8193-a895a8940f67";
         String urlImg = "https://yt3.ggpht.com/Vn34V_RSZY7jBw3UzivU_93m35T-___z2ZSDkV2I41Z30RnQqGhtQ0O8RCeNzHtRY1GTRbl-aiE=s48-c-k-c0x00ffffff-no-rj";
         updateImgDiaNhac(urlImg);
         prepareMediaPlayer(urlSong);
+
+
+        sb_playseekbar.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                SeekBar seekBar = (SeekBar) view;
+                int playPosition = (mediaPlayer.getDuration() / 100) * seekBar.getProgress();
+                mediaPlayer.seekTo(playPosition);
+                tv_currenttime.setText(milliSecondsToTimer(mediaPlayer.getCurrentPosition()));
+                return false;
+            }
+        });
+        mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+            @Override
+            public void onBufferingUpdate(MediaPlayer mediaPlayer, int i) {
+                sb_playseekbar.setSecondaryProgress(i);
+
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                sb_playseekbar.setProgress(0);
+                imv_playandpause.setImageResource(R.drawable.ic_play);
+                tv_currenttime.setText(R.string.zero);
+                tv_totalduration.setText(R.string.zero);
+                mediaPlayer.reset();
+                prepareMediaPlayer(urlSong);
+            }
+        });
 
     }
 
