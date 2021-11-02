22766143ea17a8a8584392060ec0c5fae639b0e2
HuyNQ <huynqh141643@fpt.edu.vn>
2021-11-02 20:38:12 +0700

    commit search

--- app/src/main/java/com/example/prmmusic/activity/MainActivity.java
+++ app/src/main/java/com/example/prmmusic/activity/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.prmmusic.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.prmmusic.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.clear();
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem searchItem = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
--- app/src/main/java/com/example/prmmusic/adapter/RecycleSongAdapter.java
+++ app/src/main/java/com/example/prmmusic/adapter/RecycleSongAdapter.java
@@ -1,88 +1,88 @@
-//package com.example.prmmusic.adapter;
-//
-//import android.annotation.SuppressLint;
-//import android.content.Context;
-//import android.content.Intent;
-//import android.util.Log;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import androidx.annotation.NonNull;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import com.bumptech.glide.Glide;
-//import com.example.prmmusic.R;
-//import com.example.prmmusic.activity.PlayListSong;
-//import com.example.prmmusic.model.Playlist;
-//import com.example.prmmusic.model.Song;
-//
-//import java.util.List;
-//
-//public class RecycleSongAdapter extends RecyclerView.Adapter<RecycleSongAdapter.MyViewHolder>{
-//    private List<Song> listSong;
-//    private Context context;
-//
-//    public RecycleSongAdapter(Context context, List<Song> listSong) {
-//        this.context = context;
-//        this.listSong = listSong;
-//    }
-//
-//    @NonNull
-//    @Override
-//    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        View v = LayoutInflater.from(context).inflate(R.layout.list_song_item, parent, false);
-//        return new MyViewHolder(v);
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull RecycleSongAdapter.MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
-//        Glide.with(context).load(listSong.get(position).getImageIcon()).into(holder.imv_image);
-//        holder.tv_name.setText(listSong.get(position).getName());
-//        holder.tv_description.setText("description");
-//        holder.imv_image.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent i = new Intent(context, PlayListSong.class);
-//                i.putExtra("playlist", playlists.get(position));
-//                context.startActivity(i);
-//            }
-//        });
-//        holder.tv_name.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent i = new Intent(context, PlayListSong.class);
-//                i.putExtra("playlist", playlists.get(position));
-//                context.startActivity(i);
-//            }
-//        });
-//
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return playlists.size();
-//    }
-//
-//
-//
-//
-//    /**
-//     * Data ViewHolder class.
-//     */
-//    public class MyViewHolder extends RecyclerView.ViewHolder {
-//        ImageView imv_image;
-//        TextView tv_name;
-//        TextView tv_description;
-//
-//        public MyViewHolder(@NonNull View itemView) {
-//            super(itemView);
-//            imv_image = itemView.findViewById(R.id.imv_playlistimage);
-//            tv_name = itemView.findViewById(R.id.tv_playlistname);
-//            tv_description = itemView.findViewById(R.id.tv_playlistdescription);
-//        }
-//    }
-//}
-//
+package com.example.prmmusic.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.prmmusic.R;
+import com.example.prmmusic.activity.PlayListSong;
+import com.example.prmmusic.model.Playlist;
+import com.example.prmmusic.model.Song;
+
+import java.util.List;
+
+public class RecycleSongAdapter extends RecyclerView.Adapter<RecycleSongAdapter.MyViewHolder>{
+    private List<Song> listSong;
+    private Context context;
+
+    public RecycleSongAdapter(Context context, List<Song> listSong) {
+        this.context = context;
+        this.listSong = listSong;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.list_song_item, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecycleSongAdapter.MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        Glide.with(context).load(listSong.get(position).getImage()).into(holder.imv_image);
+        holder.tv_name.setText(listSong.get(position).getName());
+        holder.tv_description.setText("description");
+        holder.imv_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(context, PlayListSong.class);
+                i.putExtra("playlist", listSong.get(position).getPlaylistID());
+                context.startActivity(i);
+            }
+        });
+        holder.tv_name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(context, PlayListSong.class);
+                i.putExtra("playlist", listSong.get(position).getPlaylistID());
+                context.startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listSong.size();
+    }
+
+
+
+
+    /**
+     * Data ViewHolder class.
+     */
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView imv_image;
+        TextView tv_name;
+        TextView tv_description;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imv_image = itemView.findViewById(R.id.imv_playlistimage);
+            tv_name = itemView.findViewById(R.id.tv_playlistname);
+            tv_description = itemView.findViewById(R.id.tv_playlistdescription);
+        }
+    }
+}
+
