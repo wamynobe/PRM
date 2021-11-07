f2b545056078f6965d35bcfee0079469e7a177f6
HuyNQ <huynqh141643@fpt.edu.vn>
2021-11-07 13:41:29 +0700

    commit code

--- app/src/main/java/com/example/prmmusic/activity/MainActivity.java
+++ app/src/main/java/com/example/prmmusic/activity/MainActivity.java
@@ -3,26 +3,13 @@ package com.example.prmmusic.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.core.view.MenuItemCompat;
-import androidx.fragment.app.FragmentActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.example.prmmusic.R;
 import com.example.prmmusic.adapter.SearchBaiHatAdapter;
-import com.example.prmmusic.model.Song;
-import com.example.prmmusic.service.APIService;
-import com.example.prmmusic.service.DataService;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
     SearchBaiHatAdapter searchBaiHatAdapter;
@@ -32,41 +19,8 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         setContentView(R.layout.activity_main);
 
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        menu.clear();
-        getMenuInflater().inflate(R.menu.main, menu);
-        MenuItem searchItem = menu.findItem(R.id.menu_search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
-        searchView.setOnQueryTextListener(this);
-        return true;
-    }
- private void SearchTuKhoaBaiHat(String query){
-     DataService dataService = APIService.getService();
-     Call<List<Song>> callback = dataService.getSongsByKeyword(query);
-     callback.enqueue(new Callback<List<Song>>() {
-         @Override
-         public void onResponse(Call<List<Song>> call, Response<List<Song>> response) {
-             ArrayList<Song> mangbaihat = (ArrayList<Song>) response.body();
-             if(mangbaihat.size()>0){
-                 searchBaiHatAdapter = new SearchBaiHatAdapter(MainActivity.this,mangbaihat);
-                 LinearLayoutManager linearLayoutManager = new  LinearLayoutManager(getApplicationContext());
-                 recyclerViewsearchbaihat.setLayoutManager(linearLayoutManager);
-                 recyclerViewsearchbaihat.setAdapter(searchBaiHatAdapter);
-                 txtkhongcodulieu.setVisibility(View.GONE);
-                 recyclerViewsearchbaihat.setVisibility(View.VISIBLE);
-             }else{
-                 recyclerViewsearchbaihat.setVisibility(View.GONE);
-                 txtkhongcodulieu.setVisibility(View.VISIBLE);
-             }
-         }
 
-         @Override
-         public void onFailure(Call<List<Song>> call, Throwable t) {
 
-         }
-     });
- }
     @Override
     public boolean onQueryTextSubmit(String query) {
         return false;
---
+++ app/src/main/java/com/example/prmmusic/fragment/tim_kiem.java
@@ -0,0 +1,109 @@
+package com.example.prmmusic.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.prmmusic.R;
+import com.example.prmmusic.adapter.SearchBaiHatAdapter;
+import com.example.prmmusic.model.Song;
+import com.example.prmmusic.service.APIService;
+import com.example.prmmusic.service.DataService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link tim_kiem#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class tim_kiem extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+    View view;
+    Toolbar toolbar;
+    RecyclerView recyclerViewsearchbaihat;
+    TextView txtkhongcodulieu;
+    SearchBaiHatAdapter searchBaiHatAdapter;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_tim_kiem,container,false);
+        toolbar = view.findViewById(R.id.toolbarSearchbaihat);
+        recyclerViewsearchbaihat = view.findViewById(R.id.recyclerviewSearchbaihat);
+        txtkhongcodulieu = view.findViewById(R.id.textViewkhongcodulieu);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
+        return  view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.search_view,menu);
+        MenuItem menuItem = menu.findItem(R.id.menu_search);
+        SearchView searchView =(SearchView) menuItem.getActionView();
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d("BBB",query);
+                return  true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+    private void SearchTuKhoaBaiHat(String query){
+        DataService dataService = APIService.getService();
+        Call<List<Song>> callback = dataService.getSongsByKeyword(query);
+        callback.enqueue(new Callback<List<Song>>() {
+            @Override
+            public void onResponse(Call<List<Song>> call, Response<List<Song>> response) {
+                ArrayList<Song> mangbaihat = (ArrayList<Song>) response.body();
+                if(mangbaihat.size()>0){
+                    searchBaiHatAdapter = new SearchBaiHatAdapter(getActivity(),mangbaihat);
+                    LinearLayoutManager linearLayoutManager = new  LinearLayoutManager(getActivity());
+                    recyclerViewsearchbaihat.setLayoutManager(linearLayoutManager);
+                    recyclerViewsearchbaihat.setAdapter(searchBaiHatAdapter);
+                    txtkhongcodulieu.setVisibility(View.GONE);
+                    recyclerViewsearchbaihat.setVisibility(View.VISIBLE);
+                }else{
+                    recyclerViewsearchbaihat.setVisibility(View.GONE);
+                    txtkhongcodulieu.setVisibility(View.VISIBLE);
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Song>> call, Throwable t) {
+
+            }
+        });
+    }
+
+}
