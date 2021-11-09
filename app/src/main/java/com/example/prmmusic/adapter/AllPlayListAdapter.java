ff658dfebb28eb9848b7f21bbd2e928818735532
duc <lexuanduc08029@gmail.com>
2021-11-09 22:55:39 +0700

    list song by playlist

--- app/src/main/java/com/example/prmmusic/adapter/AllPlayListAdapter.java
+++ app/src/main/java/com/example/prmmusic/adapter/AllPlayListAdapter.java
@@ -2,6 +2,7 @@ package com.example.prmmusic.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +14,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.prmmusic.R;
 import com.example.prmmusic.activity.MusicPlayList;
+import com.example.prmmusic.activity.PlaylistActivity;
 import com.example.prmmusic.model.Album;
 import com.example.prmmusic.model.Playlist;
+import com.example.prmmusic.model.Song;
+import com.example.prmmusic.service.APIService;
+import com.example.prmmusic.service.DataService;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 // thanh lam class nao
 public class AllPlayListAdapter extends RecyclerView.Adapter<AllPlayListAdapter.ViewHolder> {
     Context context;
@@ -42,6 +53,27 @@ public class AllPlayListAdapter extends RecyclerView.Adapter<AllPlayListAdapter.
         Picasso.get().load(playlist.getImageIcon()).into(holder.imageViewAllPlayList);
         holder.textViewAllPlaylist.setText(playlist.getName());
         holder.textviewPlaylistDesc.setText(playlist.getId());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DataService dataService = APIService.getService();
+                Call<List<Song>> call = dataService.getSongsFromPlaylist(playlist.getId());
+                call.enqueue(new Callback<List<Song>>() {
+                    @Override
+                    public void onResponse(Call<List<Song>> call, Response<List<Song>> response) {
+                        Intent intent = new Intent(v.getContext(), PlaylistActivity.class);
+                        intent.putExtra("playlist", playlist);
+                        intent.putParcelableArrayListExtra("songs",
+                                (ArrayList<? extends Parcelable>) response.body());
+                        context.startActivity(intent);
+                    }
+                    @Override
+                    public void onFailure(Call<List<Song>> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
 
     }
 
